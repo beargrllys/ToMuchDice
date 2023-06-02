aa6f39913acf806f239135c362e2fae31d9a3931
YunJiSang <31590576+beargrllys@users.noreply.github.com>
2023-06-02 22:19:50 +0900

    Update Card.cs

--- Assets/Script/Card.cs
+++ Assets/Script/Card.cs
@@ -4,7 +4,39 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
+    private enum CARD_VAL
+    {
+        ATK1 = 0,
+        ATK2,
+        ATK3,
+        ATK4,
+        ATK5,
 
+        ATK6 = 1,
+        ATK7 = 1,
+        ATK8 = 1,
+        ATK9 = 1,
+        ATK10 = 1,
+
+        ATK1 = 0,
+        ATK2 = 1,
+        ATK2 = 1,
+        ATK2 = 1,
+        ATK2 = 1,
+
+        ATK2 = 1,
+        ATK2 = 1,
+        ATK2 = 1,
+        ATK2 = 1,
+        ATK2 = 1,
+
+        ATK2 = 1,
+        ATK2 = 1,
+        ATK2 = 1,
+        ATK2 = 1,
+        ATK2 = 1,
+
+    }
     // Start is called before the first frame update
     void Start()
     {
